cb9d9e0ee5e430e766be34009a0cf9780f73513d
Prakash Jha <109400973+prakash-on-git@users.noreply.github.com>
2024-05-28 10:49:59 +0530

    Update useFetch.jsx

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,28 +3,16 @@ import { useEffect, useState } from "react";
 export const useFetch = (apiPath, queryTerm = "") => {
     const [data, setData] = useState([]);
     const url = `https://api.themoviedb.org/3${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
-    // const url =  `https://api.themoviedb.org/3${apiPath}?api_key=945a06584d09b3cc1f58449d053d4cf0`;
 
-    // useEffect(() => {
-    //     async function fetchMovies() {
-    //         const response = await fetch(url);
-    //         const data = await response.json();
-    //         console.log(data);
-    //         setData(data.results);
-    //     }
-    //     fetchMovies();
-    // }, [url])
     useEffect(() => {
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setData(data.results);
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-    }, [url]);
+        async function fetchMovies() {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            setData(data.results);
+        }
+        fetchMovies();
+    }, [url])
 
     return (
         { data }
